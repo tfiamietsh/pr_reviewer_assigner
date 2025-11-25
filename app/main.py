cec7bf7a7bb451bb01e08d9052189460b0709255
tfiamietsh <ya.touka2017@yandex.ru>
2025-11-26 00:54:10 +0300

    Add drops in startup

--- app/main.py
+++ app/main.py
@@ -8,4 +8,5 @@ app = FastAPI(title="PR Reviewer Assignment Service")
 @app.on_event("startup")
 async def startup():
     async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
