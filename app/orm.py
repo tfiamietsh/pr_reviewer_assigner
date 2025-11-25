3368ee8719fd65a1c3498b9cd07bc12084358c3e
tfiamietsh <ya.touka2017@yandex.ru>
2025-11-26 00:53:34 +0300

    Fix orms

--- app/orm.py
+++ app/orm.py
@@ -1,47 +1,56 @@
-from sqlalchemy import Column, String, Boolean, Enum
-from sqlalchemy import DateTime, Table, ForeignKey
+from typing import Optional
+from datetime import datetime
+from sqlalchemy import DateTime, ForeignKey
 from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.orm import Mapped, mapped_column
 from .enums import PullRequestStatus
 
 Base = declarative_base()
 
 
-pull_request_reviewers_table = Table(
-    "pull_request_reviewers",
-    Base.metadata,
-    Column(
-        "pull_request_id",
-        String,
+class ReviewersOrm(Base):
+    __tablename__ = "reviewers"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    pull_request_id: Mapped[str] = mapped_column(
         ForeignKey("pull_requests.pull_request_id")
-    ),
-    Column("user_id", String, ForeignKey("users.user_id"))
-)
+    )
+    user_id: Mapped[str] = mapped_column(
+        ForeignKey("users.user_id")
+    )
 
 
 class UserOrm(Base):
     __tablename__ = "users"
 
-    user_id = Column(String, primary_key=True)
-    username = Column(String, nullable=False)
-    is_active = Column(Boolean, nullable=False)
-    team = relationship("TeamOrm", back_populates="users")
-    authoring_pull_requests = relationship(
-        "PullRequestOrm",
-        back_populates="author"
+    user_id: Mapped[str] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column()
+    is_active: Mapped[bool] = mapped_column()
+    team_name: Mapped[Optional[str]] = mapped_column(
+        ForeignKey("teams.team_name")
     )
-    reviewing_pull_requests = relationship(
-        "PullRequestOrm",
-        secondary=pull_request_reviewers_table,
-        back_populates="assigned_reviewers",
-        lazy="select"
+    team: Mapped[Optional["TeamOrm"]] = relationship(
+        "TeamOrm", back_populates="members"
     )
+    authoring_pull_requests: Mapped[Optional[list["PullRequestOrm"]]] = \
+        relationship(
+            "PullRequestOrm",
+            back_populates="author",
+            lazy="select"
+        )
+    reviewing_pull_requests: Mapped[Optional[list["PullRequestOrm"]]] = \
+        relationship(
+            "ReviewersOrm",
+            back_populates="user_id",
+            lazy="select"
+        )
 
 
 class TeamOrm(Base):
     __tablename__ = "teams"
 
-    team_name = Column(String, primary_key=True)
-    members = relationship(
+    team_name: Mapped[str] = mapped_column(primary_key=True)
+    members: Mapped[list["UserOrm"]] = relationship(
         "UserOrm",
         back_populates="team",
         lazy="select"
@@ -51,20 +60,17 @@ class TeamOrm(Base):
 class PullRequestOrm(Base):
     __tablename__ = "pull_requests"
 
-    pull_request_id = Column(String, primary_key=True)
-    pull_request_name = Column(String, nullable=False)
-    status = Column(Enum(PullRequestStatus), nullable=False)
-    author_id = Column(String, ForeignKey("users.user_id"))
-    author = relationship(
-        "UserOrm",
-        back_populates="authoring_pull_requests",
-        lazy="select"
+    pull_request_id: Mapped[str] = mapped_column(primary_key=True)
+    pull_request_name: Mapped[str] = mapped_column()
+    status: Mapped[PullRequestStatus] = mapped_column()
+    author_id: Mapped[str] = mapped_column(ForeignKey("users.user_id"))
+    author: Mapped["UserOrm"] = relationship(
+        "UserOrm", back_populates="authoring_pull_requests"
     )
-    created_at = Column(DateTime, nullable=True)
-    merged_at = Column(DateTime, nullable=True)
+    created_at: Mapped[Optional[datetime]] = mapped_column()
+    merged_at: Mapped[Optional[datetime]] = mapped_column()
     assigned_reviewers = relationship(
-        "UserOrm",
-        secondary=pull_request_reviewers_table,
-        back_populates="reviewing_pull_requests",
+        "ReviewersOrm",
+        back_populates="pull_request_id",
         lazy="select"
     )
